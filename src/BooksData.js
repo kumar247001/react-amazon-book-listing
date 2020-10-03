ae8a3429cd6f3f78e443671636e37532220fb0e5
Kumar Gaurav <kumar247001@gmail.com>
2020-10-04 00:18:32 +0530

    Added new data and style changes

--- src/BooksData.js
+++ src/BooksData.js
@@ -1,72 +1,139 @@
 const Booksdata = [
-    {
-      id: 1,
-      img:
-        "https://images-eu.ssl-images-amazon.com/images/I/41hv25yqOCL._AC_SX368_.jpg",
-      name: "Red, White & Royal Blue: A Novel",
-      author: "Casey McQuiston",
-      currency: "₹",
-      original_price: "499.00",
-      discounted_price: "397.00",
-      rating: 3.5
-    },
-    {
-      id: 2,
-      img:
-        "https://images-eu.ssl-images-amazon.com/images/I/31DnhP887vL._AC_SX368_.jpg",
-      name: "Chalta-Phirta Pret । चलता-फिरता प्रेत",
-      author: "Manav Kaul",
-      currency: "₹",
-      original_price: "",
-      discounted_price: "199.00",
-      rating: 4.0
-    },
-    {
-      id: 3,
-      img:
-        "https://images-eu.ssl-images-amazon.com/images/I/51UNiQJBLEL._AC_SX368_.jpg",
-      name: "Legend of Suheldev: The King Who Saved India",
-      author: "Amish",
-      currency: "₹",
-      original_price: "399.00",
-      discounted_price: "297.00",
-      rating: 2.5
-    },
-    {
-      id: 4,
-      img:
-        "https://images-eu.ssl-images-amazon.com/images/I/51ApvliJSYL._AC_SX368_.jpg",
-      name: "Hiranyakashyap: The Narasimha Trilogy Book 2",
-      author: "Kevin Missal",
-      currency: "₹",
-      original_price: "250.00",
-      discounted_price: "156.00",
-      rating: 4.0
-    },
-    {
-      id: 5,
-      img:
-        "https://images-eu.ssl-images-amazon.com/images/I/41MSTTTkMAL._AC_SX368_.jpg",
-      name:
-        "Too Much and Never Enough: How my Family created the most dangerous man",
-      author: "Mary L. Trump",
-      currency: "₹",
-      original_price: "699.00",
-      discounted_price: "499.00",
-      rating: 2.5
-    },
-    {
-      id: 6,
-      img:
-        "https://images-eu.ssl-images-amazon.com/images/I/51x35uDVGVL._AC_SX368_.jpg",
-      name: "The Girl who got Labelled: Others",
-      author: "Abhishek Vichare",
-      currency: "₹",
-      original_price: "250.00",
-      discounted_price: "200.00",
-      rating: 1.5
-    }
-  ];
-  
-  export default Booksdata;
-  
+  {
+    id: 1,
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/41hv25yqOCL._AC_SX368_.jpg",
+    name: "Red, White & Royal Blue: A Novel",
+    author: "Casey McQuiston",
+    currency: "₹",
+    original_price: "499.00",
+    discounted_price: "397.00",
+    rating: 3.5
+  },
+  {
+    id: 2,
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/31DnhP887vL._AC_SX368_.jpg",
+    name: "Chalta-Phirta Pret । चलता-फिरता प्रेत",
+    author: "Manav Kaul",
+    currency: "₹",
+    original_price: "",
+    discounted_price: "199.00",
+    rating: 4.0
+  },
+  {
+    id: 3,
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/51UNiQJBLEL._AC_SX368_.jpg",
+    name: "Legend of Suheldev: The King Who Saved India",
+    author: "Amish",
+    currency: "₹",
+    original_price: "399.00",
+    discounted_price: "297.00",
+    rating: 2.5
+  },
+  {
+    id: 4,
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/51ApvliJSYL._AC_SX368_.jpg",
+    name: "Hiranyakashyap: The Narasimha Trilogy Book 2",
+    author: "Kevin Missal",
+    currency: "₹",
+    original_price: "250.00",
+    discounted_price: "156.00",
+    rating: 4.0
+  },
+  {
+    id: 5,
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/41MSTTTkMAL._AC_SX368_.jpg",
+    name:
+      "Too Much and Never Enough: How my Family created the most dangerous man",
+    author: "Mary L. Trump",
+    currency: "₹",
+    original_price: "699.00",
+    discounted_price: "499.00",
+    rating: 2.5
+  },
+  {
+    id: 6,
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/51x35uDVGVL._AC_SX368_.jpg",
+    name: "The Girl who got Labelled: Others",
+    author: "Abhishek Vichare",
+    currency: "₹",
+    original_price: "250.00",
+    discounted_price: "200.00",
+    rating: 1.5
+  },
+  {
+    id: 7,
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/41NoYkf9rxL._AC_SX368_.jpg",
+    name: "Draupadi - India's First Daughter",
+    author: "Vamshi Krishna",
+    currency: "₹",
+    original_price: "225.00",
+    discounted_price: "220.00",
+    rating: 4.5
+  },
+  {
+    id: 8,
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/51gIAlgY8LL._AC_SX368_.jpg",
+    name: "Three Impossible Wishes",
+    author: "Anmol Malik",
+    currency: "₹",
+    original_price: "299.00",
+    discounted_price: "224.00",
+    rating: 2.8
+  },
+  {
+    id: 9,
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/517dxLBN9eL._AC_SX368_.jpg",
+    name: "Once There Was Me: The Extraordinary Life of an Unknown Indian",
+    author: "Bobby Sachdeva",
+    currency: "₹",
+    original_price: "450.00",
+    discounted_price: "333.00",
+    rating: 3.5
+  },
+  {
+    id: 10,
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/51-D-WzfrzL._AC_SX368_.jpg",
+    name: "Walk the Wire (Amos Decker series)",
+    author: "David Baldacci",
+    currency: "₹",
+    original_price: "650.00",
+    discounted_price: "405.00",
+    rating: 4.2
+  },
+  {
+    id: 11,
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/51iYmj8+HCL._AC_SX368_.jpg",
+    name:
+      "Too Much and Never Enough: How my Family created the most dangerous man",
+    author: "John Grisham",
+    currency: "₹",
+    original_price: "399.00",
+    discounted_price: "302.00",
+    rating: 3.3
+  },
+  {
+    id: 12,
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/41N9nr36rjL._AC_SX368_.jpg",
+    name: "My Dark Vanessa",
+    author: "Kate Elizabeth Russell",
+    currency: "₹",
+    original_price: "499.00",
+    discounted_price: "357.00",
+    rating: 3.5
+  }
+];
+
+export default Booksdata;
+
